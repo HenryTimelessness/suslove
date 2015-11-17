52531a702378a7dd5e6cd3198416c66770687afc
henrychuahb-mac <henrychuahb@gmail.com>
2015-11-17 21:05:50 +0800

    Add wordpress config

---
+++ wp-config.php
@@ -0,0 +1,12 @@
+<?php
+define('DB_NAME', getenv('WP_DB_NAME'));
+
+/** MySQL database username */
+define('DB_USER', getenv('WP_DB_USER'));
+
+/** MySQL database password */
+define('DB_PASSWORD', getenv('WP_DB_PASS'));
+
+/** MySQL hostname */
+define('DB_HOST', getenv('WP_DB_HOST'));
+?>
